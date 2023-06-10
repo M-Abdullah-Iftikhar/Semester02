f70375fdc30a9406c07c4328d8f8545137721a87
MUGHAL RAJ <muhammadabdullahmughal740@gmail.com>
2023-06-10 20:03:20 +0500

    OOP

---
+++ Week07/DeckOfCards/DeckOfCards/BL/Card.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.BL
+{
+    class Card
+    {
+        private int value;
+        private int suit;
+
+        public Card(int value,int suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+        public void setValue(int value)
+        {
+            this.value = value;
+        }
+        public int getSuit()
+        {
+            return suit;
+        }
+        public void setSuit(int suit)
+        {
+            this.suit = suit;
+        }
+
+        public string getValueAsString()
+        {
+            if (value == 1)
+            {
+                return "Ace";
+            }
+            if (value == 11)
+            {
+                return "Jack";
+            }
+            if (value == 12)
+            {
+                return "Queen";
+            }
+            if (value == 13)
+            {
+                return "King";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
+        public string getSuitAsString()
+        {
+            if (suit == 1)
+            {
+                return "Clubs";
+            }
+            if (suit == 2)
+            {
+                return "Diamonds";
+            }
+            if (suit == 3)
+            {
+                return "Spades";
+            }
+            else
+            {
+                return "Hearts";
+            }
+        }
+
+        public string toString()
+        {
+            return getValueAsString() + "of" + getSuitAsString();
+        }
+    }
+}
---
+++ Week07/DeckOfCards/DeckOfCards/BL/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.BL
+{
+    class Deck
+    {
+        private int count;
+
+        private Card[] deck = new Card[52];
+
+        public Deck()
+        {
+            count = 0;
+            for(int x = 1;x<=4;x++)
+            {
+                for(int y = 1; y<= 13; y++)
+                {
+                    deck[count] = new Card(y, x);
+                    count++;
+                }
+            }
+        }
+    }
+}
